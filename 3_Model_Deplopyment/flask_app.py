bf1d6ed2b0bec1a4a943ac94f63635b692dcc117
Darien <darien.thayne@gmail.com>
2020-06-18 19:08:27 -0400

    Updated the 0/1 to yes/no and added some text at the bottom after submission

--- 3_Model_Deplopyment/flask_app.py
+++ 3_Model_Deplopyment/flask_app.py
@@ -7,7 +7,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', colours)
 
 @app.route('/predict',methods=['POST', 'GET'])
 def predict():
@@ -19,8 +19,25 @@ def predict():
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Suggested Listing Price is $ {}'.format(output))
+    
+    #Convert the 0 and 1 back to Yes / No
+    yn = {'1': 'Yes','0': 'No'}
+    
+    home_yn = yn[str(int_features[0])]
+    manhattan_yn = yn[str(int_features[1])]
+    accomidation = int_features[2]
+    bedrooms = int_features[3]
+    bathrooms = int_features[4]
+    guests_included = int_features[5]
+    
+    return render_template('index.html', 
+                           prediction_text=output,
+                           home_yn=home_yn,
+                           manhattan_yn=manhattan_yn,
+                           accomidation=accomidation,
+                           bedrooms=bedrooms,
+                           bathrooms=bathrooms,
+                           guests_included=guests_included)
 
 
 if __name__ == "__main__":
